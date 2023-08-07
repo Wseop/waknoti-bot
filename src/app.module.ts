27b299a3686c0c59aafde9fa859ea9ce872bd226
Wseop <crazyz2000@naver.com>
2023-08-07 11:56:13 +0900

    necord 세팅 & ncu

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { IntentsBitField } from 'discord.js';
+import { NecordModule } from 'necord';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    NecordModule.forRoot({
+      token: process.env.BOT_TOKEN,
+      intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.MessageContent,
+      ],
+      development: [process.env.GUILD_ID],
+    }),
+  ],
 })
 export class AppModule {}
