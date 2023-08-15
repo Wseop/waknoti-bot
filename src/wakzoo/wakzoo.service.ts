9715cf9d12ff2ec5730df22cee901fe696432f43
Wseop <crazyz2000@naver.com>
2023-08-15 22:23:50 +0900

    error handling

--- src/wakzoo/wakzoo.service.ts
+++ src/wakzoo/wakzoo.service.ts
@@ -23,7 +23,7 @@ export class WakzooService {
   async getMembersArticles(): Promise<Article[]> {
     const articles: Article[] = [];
     const browser = await puppeteer.launch({
-      headless: 'new',
+      headless: false,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
       executablePath: 'google-chrome-stable',
     });
@@ -31,46 +31,50 @@ export class WakzooService {
     await page.goto(this.url);
 
     // 전체글보기
-    await page.waitForSelector('#menuLink0');
-    await page.click('#menuLink0');
+    try {
+      await page.waitForSelector('#menuLink0');
+      await page.click('#menuLink0');
 
-    // 사이트 로딩을 위해 3초 대기
-    await new Promise((_) => setTimeout(_, 3000));
+      // 사이트 로딩 대기
+      await new Promise((_) => setTimeout(_, 3000));
 
-    const cafeMainFrame = await (await page.$('#cafe_main')).contentFrame();
-    const $ = load(await cafeMainFrame.content());
+      const cafeMainFrame = await (await page.$('#cafe_main')).contentFrame();
+      const $ = load(await cafeMainFrame.content());
 
-    // 게시글 parsing
-    for (let i = 1; i <= 15; i++) {
-      const writer = $(
-        `#main-area > div:nth-child(4) > table > tbody > tr:nth-child(${i}) > td.td_name > div > table > tbody > tr > td > a`,
-      ).text();
-      const date = $(
-        `#main-area > div:nth-child(4) > table > tbody > tr:nth-child(${i}) > td.td_date`,
-      ).text();
-      const $article = $(
-        `#main-area > div:nth-child(4) > table > tbody > tr:nth-child(${i}) > td.td_article > div.board-list > div > a`,
-      );
-      const title = $article.text().replace(/\s+/g, '').replace(']', '] ');
-      const articleHref = $article.attr('href');
-      const articleIdIndex =
-        articleHref.indexOf('articleid=') + 'articleid='.length;
-      const articleId = articleHref.substring(
-        articleIdIndex,
-        articleHref.indexOf('&', articleIdIndex),
-      );
+      // 게시글 parsing
+      for (let i = 1; i <= 15; i++) {
+        const writer = $(
+          `#main-area > div:nth-child(4) > table > tbody > tr:nth-child(${i}) > td.td_name > div > table > tbody > tr > td > a`,
+        ).text();
+        const date = $(
+          `#main-area > div:nth-child(4) > table > tbody > tr:nth-child(${i}) > td.td_date`,
+        ).text();
+        const $article = $(
+          `#main-area > div:nth-child(4) > table > tbody > tr:nth-child(${i}) > td.td_article > div.board-list > div > a`,
+        );
+        const title = $article.text().replace(/\s+/g, '').replace(']', '] ');
+        const articleHref = $article.attr('href');
+        const articleIdIndex =
+          articleHref.indexOf('articleid=') + 'articleid='.length;
+        const articleId = articleHref.substring(
+          articleIdIndex,
+          articleHref.indexOf('&', articleIdIndex),
+        );
 
-      if (
-        this.members.includes(writer) &&
-        !this.articleIds.includes(articleId)
-      ) {
-        this.articleIds.push(articleId);
-        articles.push({ writer, title, date });
+        if (
+          this.members.includes(writer) &&
+          !this.articleIds.includes(articleId)
+        ) {
+          this.articleIds.push(articleId);
+          articles.push({ writer, title, date });
+        }
       }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      await browser.close();
+
+      return articles;
     }
-
-    await browser.close();
-
-    return articles;
   }
 }
